17efa8c3aee41b90d976ca581fc736ebe2ca11cc
Sam Carrillo <scarrillo578@gmail.com>
2020-07-12 23:23:01 -0500

    Add arrays, types, and comments study

---
+++ Problems/Fill the declaration/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 123456;
+
+        System.out.println(x);
+    }
+}
---
+++ Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long[] longNumbers = new long[]{100000000001L, 100000000002L, 100000000003L};
+
+        char[] array1 = new char[0];
+        char[] array2 = new char[1];
+        char[] array3 = {'a','b','c','d'};
+        //char[] array4 = new char[-1];
+        //char[] array5 = new char[10000000000000];
+
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
