dea86aea34da0a695344527697adb7bd5337ddb6
Sam Carrillo <scarrillo578@gmail.com>
2020-07-18 00:32:29 -0500

    add broken grades.java

---
+++ Problems/Grades/src/Main.java
@@ -0,0 +1,48 @@
+/*
+Sam Carrillo
+7.18.20
+--------------
+This program take an int N and then inputs
+N integers within the range 1 - 5.
+5 = A, 4 = B, 3 = C, 2 = D. We dont count F's :]
+Then outputs the total number of students with
+each grade. <D's> <C's> <B's> <A's>
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        //build scanner
+        Scanner input = new Scanner(System.in);
+
+        //initiate the number of students
+        int n = input.nextInt();
+        int i = 0;
+        int[] grades = new int[n];
+
+        //read into grades[] until n students
+        while(i < n){
+            grades[i] = input.nextInt();
+            ++i;
+        }
+
+        //create and initialize the grades
+        int a = 0, b = 0, c = 0, d = 0;
+        for(int j = 0; j < n; i++){
+            if(grades[j] == 5){
+                a++;
+            }else if(grades[j] == 4){
+                b++;
+            }else if(grades[j] == 3){
+                c++;
+            }else if(grades[j] == 2){
+                d++;
+            }
+        }
+
+        //print totals
+        System.out.print(a + b + c + d);
+    }
+}
