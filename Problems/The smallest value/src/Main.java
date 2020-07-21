ad96042f2818e9eca9b743c18f997f936d6e8d5e
Sam Carrillo <scarrillo578@gmail.com>
2020-07-20 23:31:54 -0500

    add skeleton of n! > m program.

---
+++ Problems/The smallest value/src/Main.java
@@ -0,0 +1,26 @@
+/*
+Sam Carrillo
+7.20.2020
+-------------
+It's raining.
+finds smallest number n! > m
+ */
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long m = input.nextInt(); //long
+        int n; //x!
+
+        for(int i = 0; i < m; i++){
+            //multiples back down the line
+            for(int j = 0; j < m; j++){
+                //iterates
+            }
+            //if(n! > m) get out and print? maybe a while would be better here.
+        }
+    }
+}
