65966f4249031f03c04a3d30dc05aae38303bbd9
Sam Carrillo <scarrillo578@gmail.com>
2020-07-30 23:57:07 -0500

    add g-c percentage calculator

---
+++ Problems/The percentage of G and C characters/src/Main.java
@@ -0,0 +1,24 @@
+/*
+Sam Carrillo
+7.30.2020
+--------------
+This program calculates the percentage of G characters (guanine)
+and C characters (cytosine) in the entered string.
+
+For example, in the string "acggtgttat" the percentage of
+characters G and C equals to (4/10) * 100 = 40.0
+where 4 is the number of symbols G and C, and 10 is the length of the string.
+ */
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String inString = input.nextLine();
+        char[] charArray = inString.toCharArray();
+
+
+    }
+}
