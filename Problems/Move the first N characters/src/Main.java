b9ea76ac91500f70341a98e009a18271fcf52bf5
Sam Carrillo <scarrillo578@gmail.com>
2020-08-02 01:03:34 -0500

    add skeleton for move the first n chars

--- Problems/Move the first N characters/src/Main.java
+++ Problems/Move the first N characters/src/Main.java
@@ -1,5 +1,40 @@
+/*
+Sam Carrillo
+8.1.2020
+--------------
+This program takes in a string s and
+and int n, shifts the first n characters
+of s to the end of the string.
+if n > s.length then outputs the string
+unchanged.
+ */
+
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        //create scanner object
+        Scanner input = new Scanner(System.in);
+
+        //initialize s and n from input
+        String s = input.nextLine();
+        int n = input.nextInt();
+        char[] sArray = s.toCharArray();
+        String s1 = "";
+
+        for(int i = 0; i < n; i++){
+
+            if(n > s.length()){
+                break;
+            }
+
+            //create the string to append to the end of s
+            //remove the first n characters from s
+            //append string to end of s?
+                //might need to be in conditional stmt or
+                //outside of loop
+        }
+
+        //output s1 (or whatever the modified string is called)
     }
 }
