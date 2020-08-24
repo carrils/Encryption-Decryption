9045ec06bad377829a3424576e1e659cce125b54
Sam Carrillo <scarrillo578@gmail.com>
2020-08-23 20:49:30 -0500

    clean up main crypt-decrypt program

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -16,66 +16,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String sourceString = input.nextLine();
+        //String sourceString = input.nextLine();
+        char[] chars = input.nextLine().toCharArray();
         int key = input.nextInt();
-
-        //create a character array of the String & alphabet char array
-        char[] sourceArray = sourceString.toCharArray();
-        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-        //data structure for encrypting sourceArray
-        for (int i = 0; i < sourceArray.length; i++) {
-            //ignore spaces, add in here anything you want to ignore. can be better.
-            if(sourceArray[i] == ' '){
-                continue;
-            }
-            for (int j = 0; j <= alphabet.length; j++) {
-                //make A follow Z if not found in first revolution
-                if (j == alphabet.length) {
-                    j = 0;
-                }
-                //encrypt sourceArray with the key
-                if (sourceArray[i] == alphabet[j]) {
-                    //if value after shifting > 25, adjust shifted value
-                    if (j + key > alphabet.length) {
-                        sourceArray[i] = alphabet[(j - 26) + key];
-                        break;
-                    } else {
-                        sourceArray[i] = alphabet[j + key];
-                        break;
-                    }
-                }
-            }
-        }
-
-        //print result
-        String encryptedSource = new String(sourceArray);
-        System.out.println(encryptedSource);
-    }
-}
-/* "official solution"
-IMPROVISE
-ADAPT
-OVERCOME
-LMAO
-
-package encryptdecrypt;
-
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char[] chars = scanner.nextLine().toCharArray();
-        int shift = scanner.nextInt();
 
         char a = 'a';
         char z = 'z';
         int size = 26;
 
+        //data structure for encrypting sourceArray
         for (char item : chars) {
             if (item >= a && item <= z) {
-                char shiftItem = (char) (((item - a + shift) % size) + a);
+                char shiftItem = (char) (((item - a + key) % size) + a);
                 System.out.print(shiftItem);
             } else {
                 System.out.print(item);
@@ -83,4 +35,3 @@ public class Main {
         }
     }
 }
- */
