fa60239e8769438dc564cbe84739fe863862f60c
Sam Carrillo <scarrillo578@gmail.com>
2020-08-24 22:29:17 -0500

    add encrypt and decrypt methods

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -14,29 +14,38 @@ package encryptdecrypt;
 import java.util.Scanner;
 /*
     TODO:
-    - make it read in 3 parameters
-        target operation for encryption and decryption (enc and dec)
-        message
-        integer key (shift)
     - All non-characters should be encrypted as well (use unicode table for number representation of each char)
-    - decompose program, methods for encrypting and decrypting
+    - fill out decrypt
  */
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String operation = input.nextLine();
         char[] chars = input.nextLine().toCharArray();
         int key = input.nextInt();
 
+
+    }
+
+    public static String decrypt(char[]_chars, int _key){
+        //this is encrypt in reverse first get all nonalphabetic chars encrypted
+        String result = "";
+        return result;
+    }
+
+    public static String encrypt(char[] _chars, int _key){
         char a = 'a';
         char z = 'z';
         int size = 26;
-
+        String result = "";
         //for-each loop for encrypting chars array
-        for (char item : chars) {
+        for (char item : _chars) {
             if (item >= a && item <= z) {
-                char shiftItem = (char) (((item - a + key) % size) + a);
-                System.out.print(shiftItem);
+                char shiftItem = (char) (((item - a + _key) % size) + a);
+                result += shiftItem;
             } else {
+                //this is all the non-alphabetical characters.
+                //widen to cover these to and just += shift item
                 System.out.print(item);
             }
         }
