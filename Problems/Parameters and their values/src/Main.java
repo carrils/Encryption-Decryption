4600342150f75dd412cc41aa997669c137c22320
Sam Carrillo <scarrillo578@gmail.com>
2020-09-07 22:26:43 -0500

    finish parameters and their values

--- Problems/Parameters and their values/src/Main.java
+++ Problems/Parameters and their values/src/Main.java
@@ -1,28 +1,8 @@
 class Problem {
 
     public static void main(String[] args) {
-        String temp1 = "";
-        String temp2 = "";
-        String glue = "";
-        String[] gluedItems = new String[args.length];
-        for (int i = 2; i <= args.length + 2; ++i) {
-            //maybe increment loop by 2?
-//            for (int i = 0 ;i<args.length-1;i+=2){
-//                System.out.println(args[i]+"="+args[i+1]);
-            int j = 0;
-            if (i % 2 == 0) {
-                //create temp 1 variable for arg
-                temp1 = args[i];
-            } else{
-                temp2 = args[i];
-            }
-            glue = temp1.toString() + "=" +temp2.toString();
-            gluedItems[j] += glue;
-            j++;
-        }
-
-        for(String item : gluedItems){
-            System.out.println(item);
+        for (int i = 0; i < args.length - 1; i += 2) {
+            System.out.println(args[i] + "=" + args[i + 1]);
         }
     }
 }
