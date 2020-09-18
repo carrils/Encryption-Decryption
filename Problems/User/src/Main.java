1bdfa56f3f5aa9caaa58579f3dcfd98ae1c75f39
Sam Carrillo <scarrillo578@gmail.com>
2020-09-17 23:26:22 -0500

    add User class w/ constructor

---
+++ Problems/User/src/Main.java
@@ -0,0 +1,11 @@
+class User {
+    String login;
+    String firstName;
+    String lastName;
+
+    public User(String _login, String _firstName, String _lastName) {
+        this.login = _login;
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+    }
+}
