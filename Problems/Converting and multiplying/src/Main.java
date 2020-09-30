ad51fd106298df68bf2b6642dad9570d7f4bd69f
Sam Carrillo <scarrillo578@gmail.com>
2020-09-29 23:55:49 -0500

    add converting and multiplying

---
+++ Problems/Converting and multiplying/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()) {
+            String x = input.next();
+            int y = Integer.valueOf(x);
+
+            if (x.equals("0")) {
+                break;
+            }
+
+        }
+
+    }
+}
