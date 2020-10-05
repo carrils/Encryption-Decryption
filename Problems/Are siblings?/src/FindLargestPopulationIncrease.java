c328306ddb5342c15791f799f06402a72b569040
Sam Carrillo <scarrillo578@gmail.com>
2020-10-04 23:43:09 -0500

    add diff calculation to FindLargestPopulationIncrease

--- Problems/Are siblings?/src/FindLargestPopulationIncrease.java
+++ Problems/Are siblings?/src/FindLargestPopulationIncrease.java
@@ -8,25 +8,37 @@ import java.io.FileNotFoundException;
 public class FindLargestPopulationIncrease {
     public static void main(String[] args) {
         File populationData = new File("dataset_91069.txt");
-
+        int i = 0;
         try {
             Scanner input = new Scanner(populationData);
 
             while(input.hasNext()){
-                try{
+                try {
                     //System.out.println(Arrays.toString(input.nextLine().split("\t")));
                     //int population = Integer.parseInt(fileLine[1].replaceAll(",",""));//NFE
-                    String [] fileLine = input.nextLine().split("\t");
-                    long x = (long) NumberFormat.getNumberInstance(Locale.US).parse(fileLine[1]);//PE
-                    System.out.println(x);
+                    String[] fileLine = input.nextLine().split("\t");
+                    long temp = 0; //temp variable to hold previous population
+                    long previousPopulation = 0;
+                    long diff = 0;
+                    if (i >= 1) {
+                        //skip header of file
+                        temp = (long) NumberFormat.getNumberInstance(Locale.US).parse(fileLine[1]);
+                        //System.out.println(temp);
+                        if(previousPopulation != 0){
+                            diff = temp - previousPopulation;
+                        }
+
+                    }
+                    previousPopulation = temp;
+
                 }catch(ParseException e){
                     System.out.println("<Unparseable line>");
                 }
+                i++;
             }
         } catch (FileNotFoundException e) {
             //every time you pass Scanner a File you must handle FNFE
             System.out.println("File not found: " + populationData.getName());
         }
-
     }
 }
