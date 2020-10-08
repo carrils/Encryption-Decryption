3d4b0db4a33e34a85cd5e45a7024106ddaab17c4
Sam Carrillo <scarrillo578@gmail.com>
2020-10-08 00:09:13 -0500

    add doing portion encrypt(?)

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -11,7 +11,8 @@ of the English alphabet
  */
 package encryptdecrypt;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,7 +21,11 @@ public class Main {
         int key = 0;
         char[] chars = {};//presume data is an empty string
         boolean usingData = false;
+        boolean usingIn = false;
         boolean hasOut = false;
+        String inputFile = ""; //should these be strings for the file names or actually files
+        String outPutFile = ""; //same^ and null?
+
         //parameter processing
         for (int i = 0; i < args.length - 1; i += 2) {
             //change to case switch?
@@ -35,8 +40,8 @@ public class Main {
             //If there is no -key, the program should consider that key = 0.
             //If there is no -data, and there is no -in the program should assume that the data is an empty string.
             //If there is no -out argument, the program must print data to the standard output.
-            //If there are both -data and -in arguments, your program should prefer -data over -in.
-            switch (args[i]){
+            //If there are both -data and -in arguments, your program should prefer -data over -in. (
+            switch (args[i]) {
                 case "-mode":
                     mode = args[i + 1];
                     break;
@@ -49,18 +54,16 @@ public class Main {
                     break;
                 case "-in":
                     //order might matter as in if -in before -data it would see usingdata as false
-                    if (usingData) {
-                        break;
-                    } else {
-                        //only has to initialize the file.
-                        //the try catch will be in the doing portion
-                        File inputFile = new File(args[i + 1]);
-                    }
+                    //only has to initialize the file.
+                    //the try catch will be in the doing portion
+                    usingIn = true;
+                    inputFile = args[i + 1];
+
                     break;
                 case "-out":
                     //mkdir i think
                     hasOut = true;
-                    File outPutFile = new File(args[i] + 1);
+                    outPutFile = args[i] + 1;
                     break;
             }
         }
@@ -68,13 +71,25 @@ public class Main {
         //doing portion
         if (mode.equals("enc")) {
             //System.out.println(encrypt(chars, key));
+            if (usingData && usingIn) {
+                //go into Data processing
+                encrypt(chars, key);
+                if (hasOut) {
+                    try {
+                        PrintWriter output = new PrintWriter(outPutFile);
+                    }  catch (FileNotFoundException e){
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                }
+            }
+
         } else if (mode.equals("dec")) {
             //System.out.println(decrypt(chars, key));
         }
 
     }
 
-    public static String decrypt(char[] _chars, int _key) {
+    public static void decrypt(char[] _chars, int _key) {
         //method for decrypt
         char nullChar = 0;//beginning of base ASCII table
         char delChar = 127;//ending of base ASCII table
@@ -85,10 +100,10 @@ public class Main {
             char shiftItem = (char) (((item + nullChar - _key) % size) - nullChar);
             result += shiftItem;
         }
-        return result;
+        //return result;
     }
 
-    public static String encrypt(char[] _chars, int _key) {
+    public static void encrypt(char[] _chars, int _key) {
         //method for encrypt
         char nullChar = 0; // \0
         char delChar = 127;// 007F
@@ -99,6 +114,6 @@ public class Main {
             char shiftItem = (char) (((item - nullChar + _key) % size) + nullChar);
             result += shiftItem;
         }
-        return result;
+        //return result;
     }
 }
