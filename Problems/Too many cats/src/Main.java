8d365383e38aaa9ed1d44d8128dd4167e1b6facc
Sam Carrillo <scarrillo578@gmail.com>
2020-10-27 23:13:48 -0500

    add too many cats

---
+++ Problems/Too many cats/src/Main.java
@@ -0,0 +1,19 @@
+class Cat {
+    String name;
+    int age;
+    static int counter;
+
+    public Cat(String name, int age) {
+        this.age = age;
+        this.name = name;
+        counter++;
+
+        if(counter > 5){
+            System.out.println("You have too many cats");
+        }
+    }
+
+    public static int getNumberOfCats() {
+        return counter;
+    }
+}
